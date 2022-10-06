c7f1577082436b80c9115a14094cbb58cadb9995
shuib <shuaib_97@hotmail.co.uk>
2022-10-06 20:18:46 +0100

    classmates

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //WRITE YOUR CODE BELOW
+let student = ["mohamud" , "zaid" , "yousuf" , "tomal" , "sha"]
+console.log(student)
+for (let i = 0 ;i < student.length ; i++ ){
+console.log(" Great to see you "+ 'student')
+}
